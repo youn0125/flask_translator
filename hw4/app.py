861098149bbe530b0dad03ed2e7839b0c860faa2
miyon <miyon@pdx.edu>
2019-11-15 10:31:15 -0800

    Add documentation via Docstrings to app.py

--- hw4/app.py
+++ hw4/app.py
@@ -9,18 +9,29 @@ from liststore import Liststore
 
 app = flask.Flask(__name__)       # our Flask app
 
+"""
+Create the endpoint(index.html)
+"""
 app.add_url_rule('/',
                  view_func=Index.as_view('index'),
                  methods=["GET"])
 
+"""
+Creates the endpoint(addstore.html)
+"""
 app.add_url_rule('/addstore/',
                  view_func=Addstore.as_view('addstore'),
                  methods=['GET', 'POST'])
 
+"""
+Creates the endpoint(liststore.html)
+"""
 app.add_url_rule('/liststore/',
                  view_func=Liststore.as_view('liststore'),
                  methods=['GET'])
 
-
-if __name__ == '__main__':	
-    app.run(host='0.0.0.0', port=8000, debug=True)	# port number 8000
+"""
+local host port is 8000
+"""
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
