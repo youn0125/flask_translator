7fc0f24ab25298b84862ad8e09f3b9e34f5e2ee6
miyon <miyon@pdx.edu>
2019-12-10 23:06:04 -0800

    Add text to speech and store to bucket Change app.yaml to standard Add new page to display the result of text translation and audio file

--- final/app.py
+++ final/app.py
@@ -6,14 +6,13 @@ from datetime import datetime
 import logging
 import os
 import six
-
 from flask import Flask, redirect, render_template, request
-
 
 from google.cloud import datastore
 from google.cloud import storage
 from google.cloud import speech
 from google.cloud import translate_v2 as translate
+from google.cloud import texttospeech
 
 CLOUD_STORAGE_BUCKET = os.environ.get('CLOUD_STORAGE_BUCKET')
 
@@ -24,79 +23,68 @@ Create the endpoint(index.html)
 """
 @app.route('/')
 def index():
-    # Create a Cloud Datastore client.
-    datastore_client = datastore.Client()
+    # Return a Jinja2 HTML template
+    return render_template('index.html')
 
-    # Use the Cloud Datastore client to fetch information from Datastore about
-    # each photo.
-    query = datastore_client.query(kind='Speech')
-    audio_entities = list(query.fetch())
+"""
+Translate text and store audio file to bucke
+"""
+@app.route('/trans_text', methods=['GET', 'POST'])
+def trans_text():
+    # Trans text
+    # get text the user entered
+    input_text = request.form['text']
+    # create Client object
+    translate_client = translate.Client()
+    # decode text if it's a binary type
+    if isinstance(input_text, six.binary_type):
+        transcription = transcription.decode('utf-8')
+    # get translation result by passing text and target language to client
+    # Text can also be a sequence of strings, in which case this method
+    # will return a sequence of results for each text.
+    result = translate_client.translate(input_text, target_language='en')
+    # only interested in translated text
+    translation = result['translatedText']
 
-    # Return a Jinja2 HTML template and pass in image_entities as a parameter.
-    return render_template('index.html', audio_entities=audio_entities)
 
-@app.route('/upload_speech', methods=['GET', 'POST'])
-def upload_speech():
-    #Upload file to bucket
-    speech_file = request.files['file']
+    # Translated text to speech
+    # Instantiates a client
+    client = texttospeech.TextToSpeechClient()
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.types.SynthesisInput(text=translation)
+
+    # Build the voice request, select the language code ("en-US") and the ssml
+    # voice gender ("neutral")
+    voice = texttospeech.types.VoiceSelectionParams(
+        language_code='en-US',
+        ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+
+    # Select the type of audio file you want returned
+    audio_config = texttospeech.types.AudioConfig(
+        audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+
+    # Perform the text-to-speech request on the text input with the selected
+    # voice parameters and audio file type
+    response = client.synthesize_speech(synthesis_input, voice, audio_config)
+
+    # Store into bucket
+    speech_file = 'output.mp3'
     # Create a Cloud Storage client.
     storage_client = storage.Client()
     # Get the bucket that the file will be uploaded to.
     bucket = storage_client.get_bucket(CLOUD_STORAGE_BUCKET)
     # Create a new blob and upload the file's content.
-    blob = bucket.blob(speech_file.filename)
-    blob.upload_from_string(speech_file.read(), content_type=speech_file.content_type)
+    blob = bucket.blob(speech_file)
+    blob.upload_from_string(response.audio_content, content_type='audio/mpeg')
     # Make the blob publicly viewable.
     blob.make_public()
+
+    #make entries
+    entries = [dict(iText=input_text, transText=translation, 
+        link= 'https://storage.cloud.google.com/' + CLOUD_STORAGE_BUCKET + '/'+ speech_file)]
     
-    #Create Cloud Speech client
-    client = speech.SpeechClient()
-    # specify location of speech
-    source_uri = 'gs://{}/{}'.format(CLOUD_STORAGE_BUCKET, blob.name)
-    audio = speech.types.RecognitionAudio(uri=source_uri) # need to specify speech.types
-    # set language to Turkish
-    # removed encoding and sample_rate_hertz
-    config = speech.types.RecognitionConfig(language_code='tr-TR') # need to specify speech.types
-    # get response by passing config and audio settings to client
-    response = client.recognize(config, audio)
-    #get transcription
-    transcription = response.results[0].alternatives[0].transcript
-
-    # create Client object
-    translate_client = translate.Client()
-    # decode text if it's a binary type
-    if isinstance(transcription, six.binary_type):
-        transcription = transcription.decode('utf-8')
-    # get translation result by passing text and target language to client
-    # Text can also be a sequence of strings, in which case this method
-    # will return a sequence of results for each text.
-    result = translate_client.translate(transcription, target_language='en')
-    # only interested in translated text
-    translation = result['translatedText']
-
-    # Create a Cloud Datastore client.
-    datastore_client = datastore.Client()
-    # Fetch the current date / time.
-    current_datetime = datetime.now()
-    # The kind for the new entity.
-    kind = 'Speech'
-    # The name/ID for the new entity.
-    name = blob.name
-    # Create the Cloud Datastore key for the new entity.
-    key = datastore_client.key(kind, name)
-    # Construct the new entity using the key. Set dictionary values for entity
-    # keys blob_name, storage_public_url, timestamp, transcription, and translation..
-    entity = datastore.Entity(key)
-    entity['blob_name'] = blob.name
-    entity['audio_public_url'] = blob.public_url
-    entity['timestamp'] = current_datetime
-    entity['transcription'] = transcription
-    entity['translation'] = translation
-    # Save the new entity to Datastore.
-    datastore_client.put(entity)
-
-    # Redirect to the home page.
-    return redirect('/')
+    # Go to transtext.html.
+    return render_template('transtext.html',entries=entries)
 
 @app.errorhandler(500)
 def server_error(e):
